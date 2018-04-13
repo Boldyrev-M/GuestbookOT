b6dc7df0f9c1aa3c8ebaab17c54099096ca418d4
MaxB <maxikb@mail.ru>
2018-04-13 16:43:00 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -2,10 +2,15 @@
 // параметры соединения с БД
 
 $db_config = array(
-    'db_address' => "127.0.0.1",
-    'db_user' => "root",
-    'db_password' => "root",
-    'db_name' => "guestbook",
-    'db_port' => 8889
+//    'db_address' => "127.0.0.1",
+//    'db_user' => "root",
+//    'db_password' => "root",
+//    'db_name' => "guestbook",
+    'db_address' => "localhost",
+    'db_user' => "w1_guestbook",
+    'db_password' => "NDUpdajFLPtI4GkMr8d9",
+    'db_name' => "w1_guestbook",
+//    'db_port' => 8889
+    'db_port' =>  3306
 );
     
